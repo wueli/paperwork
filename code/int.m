179d71e976d1843cd704859f64c46cee57258c64
Ueli <wueli@ethz.ch>
2012-12-10 16:56:07 +0100

    sachen lschen

--- code/int.m
+++ code/int.m
@@ -8,13 +8,7 @@ if sqrt(A(i,3)^2+A(i,4)^2)>(1.2*A(i,7))                %limits maximum of veloci
     A(i,3)=A(i,3)/sqrt(A(i,3)^2+A(i,4)^2)*A(i,7)*1.2;      
     A(i,4)=A(i,4)/sqrt(A(i,3)^2+A(i,4)^2)*A(i,7)*1.2;      
 end
-
-% %random angle for rotating of velocity vector
-% alpha=randn*arc+tend; 
-% a=[cos(alpha) -sin(alpha); sin(alpha) cos(alpha)]*[A(i,3); A(i,4)];
-% A(i,3)=a(1);
-% A(i,4)=a(2);
-     
+    
     A(i,1)=A(i,1)+A(i,3)*t;    %dx=F*dt + x(t-dt)
     A(i,2)=A(i,2)+A(i,4)*t;
      
